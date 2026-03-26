7a39a3092a3781b12782b7e9d577c17f434d5659
Althea <hapathea0304@gmail.com>
2026-03-27 00:58:06 +0800

    Simplify User model

--- app/src/main/java/com/example/hanaparal/models/User.kt
+++ app/src/main/java/com/example/hanaparal/models/User.kt
@@ -5,11 +5,4 @@ data class User(
     val name: String = "",
     val course: String = "",
     val email: String = ""
-) {
-    fun toMap(): Map<String, Any> = mapOf(
-        "uid" to uid,
-        "name" to name,
-        "course" to course,
-        "email" to email
-    )
-}
+)
