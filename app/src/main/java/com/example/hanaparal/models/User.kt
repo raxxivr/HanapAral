d14f63b456603b0f5416d01955b36e122f7eabec
Althea <hapathea0304@gmail.com>
2026-03-26 22:20:39 +0800

    Create User data class with uid, name, course, and email fields and add toMap() function for Firestore integration

--- app/src/main/java/com/example/hanaparal/models/User.kt
+++ app/src/main/java/com/example/hanaparal/models/User.kt
@@ -3,7 +3,13 @@ package com.example.hanaparal.models
 data class User(
     val uid: String = "",
     val name: String = "",
-    val email: String = "",
     val course: String = "",
-    val program: String = ""
-)
+    val email: String = ""
+) {
+    fun toMap(): Map<String, Any> = mapOf(
+        "uid" to uid,
+        "name" to name,
+        "course" to course,
+        "email" to email
+    )
+}
